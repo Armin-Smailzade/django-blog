dd3984277eb4d393fc77213100b9aa35bc8a5baa
armin <armin.esmaeilzadeh@gmail.com>
2016-01-24 15:57:58 -0800

    Forms

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 	
 	url(r'^$', views.post_list, name='post_list'),
-	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail') #Django will take everything that you place here and transfer it to a view as a variable called pk
-
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'), #Django will take everything that you place here and transfer it to a view as a variable called pk
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit')
 ]
